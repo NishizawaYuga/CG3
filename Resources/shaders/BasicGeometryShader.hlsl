51b5b0d66ca430853cd8c9376e6c1e927d8a83a7
NishizawaYuga <k021g1246@g.neec.ac.jp>
2023-01-16 14:46:14 +0900

    点からジオメトリ生成

--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,17 +1,38 @@
 #include "BasicShaderHeader.hlsli"
 
+//点の入力から、点を出力
+//[maxvertexcount(1)]
+//void main(
+//	point VSOutput input[1] : SV_POSITION,
+//	inout TriangleStream< GSOutput > output
+//)
+//{
+//	GSOutput element;
+//	//頂点情報
+//	element.svpos = input[0].svpos;
+//	element.normal = input[0].normal;
+//	element.uv = input[0].uv;
+//	output.Append(element);
+//}
+
+//点の入力から、三角形を出力
 [maxvertexcount(3)]
 void main(
-	triangle VSOutput input[3] : SV_POSITION,
+	point VSOutput input[1] : SV_POSITION,
 	inout TriangleStream< GSOutput > output
 )
 {
-	for (uint i = 0; i < 3; i++)
-	{
-		GSOutput element;
-		element.svpos = input[i].svpos;
-		element.normal = input[i].normal;
-		element.uv = input[i].uv;
-		output.Append(element);
-	}
+	GSOutput element;
+	//共通
+	element.normal = input[0].normal;
+	element.uv = input[0].uv;
+	//1点目
+	element.svpos = input[0].svpos;
+	output.Append(element);
+	//2点目
+	element.svpos = input[0].svpos + float4(10.0f, 10.0f, 0, 0);
+	output.Append(element);
+	//3点目
+	element.svpos = input[0].svpos + float4(10.0f, 0, 0, 0);
+	output.Append(element);
 }
